c3aecf699906bca580cd9fc2dbb856b27b559db1
sa6uhi <sabuhi.nazarov10@gmail.com>
2025-04-17 23:14:52 +0400

    SortAnArray.c edit

--- c-programming-full-course-bro-code/SortAnArray.c
+++ c-programming-full-course-bro-code/SortAnArray.c
@@ -35,7 +35,7 @@ void printArray(int array[], int size)
 
 int main()
 {
-    int array[] = {9, 1, 8, 2, 7, 3, 6, 4, 5};`
+    int array[] = {9, 1, 8, 2, 7, 3, 6, 4, 5};
     int size = sizeof(array) / sizeof(array[0]);
 
     sort(array, size);
