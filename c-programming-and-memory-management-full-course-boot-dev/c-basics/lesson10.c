ae2d55ec669c1e39f713a066f68137f3a90a9bb6
Sabuhi <sabuhi.nazarov11@gmail.com>
2025-04-18 23:29:43 +0400

    added files

---
+++ c-programming-and-memory-management-full-course-boot-dev/c-basics/lesson10.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  int sneklang_int_size = 64;
+  sneklang_int_size = 32;
+  printf("Sneklang int size: %d bits\n", sneklang_int_size);
+  return 0;
+}
---
+++ c-programming-and-memory-management-full-course-boot-dev/c-basics/lesson13.c
@@ -0,0 +1,7 @@
+int get_integer(void) {
+    return 42;
+}
+
+void print_integer(int x) {
+    printf("this is an int: %d", x);
+}
