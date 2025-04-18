63046c5191dc4ca2a0ccc8b684122cd61a258ab9
Sabuhi <sabuhi.nazarov11@gmail.com>
2025-04-18 23:14:51 +0400

    added files

---
+++ c-programming-and-memory-management-full-course-boot-dev/c-basics/lesson2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  printf("starting sneklang tools\n");
+  // does_not_exists("uh oh");
+  printf("finished sneklang tools\n");
+  return 0;
+}
---
+++ c-programming-and-memory-management-full-course-boot-dev/c-basics/lesson4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+char *will_never_hear_again =
+      "Hey TJ, when is the memory course in C gonna be done?";
+
+  // don't touch below this line
+  printf("%s\n", will_never_hear_again);
+  return 0;
+}
