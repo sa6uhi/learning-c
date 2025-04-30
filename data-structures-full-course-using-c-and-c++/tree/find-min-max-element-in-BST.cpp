2c26dca8ab514967060da897ac1eaffd4f2aec06
sa6uhi <sabuhi.nazarov10@gmail.com>
2025-04-30 15:43:27 +0400

    finding min element in bst added findMin() function

---
+++ data-structures-full-course-using-c-and-c++/tree/find-min-max-element-in-BST.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+typedef struct bstNode_t
+{
+    int data;
+    struct bstNode_t *left;
+    struct bstNode_t *right;
+} bstNode_t;
+
+int findMin(bstNode_t *root)
+{
+    if (root == NULL)
+    {
+        std::cout << "Tree is empty" << std::endl;
+        return -1; // Tree is empty
+    }
+    while (root->left != NULL)
+    {
+        root = root->left;
+    }
+    return root->data;
+}
+
